f1b37c3783033c85b5fa412e22530c8a3fab1446
Gary Jipp <github@accu.ca>
2021-05-11 21:13:57 -0700

    Update

--- src/UserLogin.js
+++ src/UserLogin.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext } from 'react';
 import { authContext } from './AuthProvider';
 import Counter from './Counter';
 
