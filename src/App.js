5ac9139437606bac1d6d2714d6517521552e4df5
Gary J <github@accu.ca>
2021-06-07 20:19:08 -0700

    Edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'App.css';
 
 export default function App() {
 
-  // We can use this because we even wrapped <App> in our Provider (in index.js)
+  // We can use this because we wrapped <App> in AuthProvider (in index.js)
   const { auth } = useContext(authContext);
 
   return (
