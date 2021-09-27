0381183d3e91d61c1ca345c260269f32020d075d
Gary J <github@accu.ca>
2021-09-26 19:26:48 -0700

    Update

--- src/providers/StateProvider.js
+++ src/providers/StateProvider.js
@@ -1,27 +1,25 @@
 import { createContext, useState } from 'react';
 
+export const stateContext = createContext();
+
 export default function StateProvider(props) {
 
   // Here is our Shared State Object
-  const [state, setState] = useState({
-    counter: 0,
-    item1: 'more stuff 1',
-    item2: 'more stuff 2',
-  });
+  const [counter, setCounter] = useState(0);
 
   // Functions to change  the counter state item
-  const increment = function () {
-    setState({ ...state, counter: state.counter + 1 });
+  const increment = function() {
+    setCounter(counter + 1);
   };
-  const decrement = function () {
-    setState({ ...state, counter: state.counter - 1 });
+  const decrement = function() {
+    setCounter(counter - 1);
   };
-  const clear = function () {
-    setState({ ...state, counter: 0 });
+  const clear = function() {
+    setCounter(0);
   };
 
   // This list can get long with a lot of functions.  Reducer may be a better choice
-  const providerData = { state, counter: state.counter, increment, decrement, clear };
+  const providerData = { counter, increment, decrement, clear };
 
   // We can now use this as a component to wrap anything 
   // that needs our state
@@ -32,4 +30,3 @@ export default function StateProvider(props) {
   );
 };
 
-export const stateContext = createContext();
