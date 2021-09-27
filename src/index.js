2daaa2c5542face764eb2d98fd6db8951c19f233
Gary J <github@accu.ca>
2021-09-26 18:39:35 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import 'index.css';
 import App from 'App';
 
-// check if user still logged in.
-
-// Wrap entire app in AuthProvider so its also available to <App>
 ReactDOM.render(
   <App />,
   document.getElementById('root')
