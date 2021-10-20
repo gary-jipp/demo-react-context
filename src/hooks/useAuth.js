aa17868b6a5562648b6e8f00988c05890996cea6
Gary J <github@accu.ca>
2021-10-19 20:53:02 -0700

    Update

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 const useAuth = function() {
-
   const [auth, setAuth] = useState(false);
   const [user, setUser] = useState(null);
 
---
+++ src/providers/AuthProvider.js
@@ -0,0 +1,30 @@
+import { createContext, useState } from 'react';
+
+export const authContext = createContext();
+
+export default function AuthProvider(props) {
+  const [auth, setAuth] = useState(false);
+  const [user, setUser] = useState(null);
+
+  // Perform login process for the user & save authID, etc
+  const login = function(email, password) {
+    setAuth(true);
+    const id = "1234-1234-1234";  // Some random userId
+    setUser({ email, id, name: "Test User" });
+  };
+
+  const logout = function() {
+    setAuth(false);
+    setUser(null);
+  };
+
+  // authContext will expose these items
+  const userData = { auth, user, login, logout };
+
+  // We can use this component to wrap any content we want to share this context
+  return (
+    <authContext.Provider value={userData}>
+      {props.children}
+    </authContext.Provider>
+  );
+};
