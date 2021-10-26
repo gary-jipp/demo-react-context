69d187d1a05fc4ad2dcbd47be30c68ee7036abe9
Gary J <github@accu.ca>
2021-10-25 19:17:58 -0700

    Update

--- src/Counter.js
+++ src/Counter.js
@@ -4,6 +4,17 @@ import 'App.css';
 export default function Counter() {
   const [counter, setCounter] = useState(0);
 
+  // Functions to change  the counter state item
+  const increment = function() {
+    setCounter(counter + 1);
+  };
+  const decrement = function() {
+    setCounter(counter - 1);
+  };
+  const clear = function() {
+    setCounter(0);
+  };
+
   return (
     <div>
       Counter: <span className="counter"> {counter} </span>
