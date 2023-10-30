6dc86b2ffb392a6f4a104070f5d200b6c3baad13
gary <github@accu.ca>
2023-10-30 10:43:59 -0700

    Update

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -7,15 +7,15 @@ export default function Info(props) {
   // Show user Info
   return (
     <div>
-      <p className="UserInfo">
+      <div className="UserInfo">
         <div>You are logged in</div>
         <div>Email: {user.email}</div>
         <div>Name: {user.name}</div>
         <div>UserId: {user.id}</div>
-      </p >
-      <p>
+      </div >
+      <div>
         <button type="button" onClick={logout}>Logout</button>
-      </p>
+      </div>
 
       <Counter />
     </div>
