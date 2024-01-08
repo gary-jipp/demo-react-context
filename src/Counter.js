a5cae320c7ffa466305ac712dbce3a03742a3a66
gary <github@accu.ca>
2024-01-08 13:27:21 -0800

    Move clear

--- src/Counter.js
+++ src/Counter.js
@@ -19,8 +19,8 @@ export default function Counter() {
     <div className="counter">
       Counter: <span className="counter"> {counter} </span>
       <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
       <button onClick={clear}>0</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
