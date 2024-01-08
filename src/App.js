73140f52a85f1ba2b0822cd50b85ea92b0a3fd62
gary <github@accu.ca>
2024-01-08 13:29:18 -0800

    remove auth state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Info from 'pages/Info';
 import Login from 'pages/Login';
 import 'App.css';
 
 export default function App() {
-  const [auth, setAuth] = useState(false);
   const [user, setUser] = useState(null);
 
   // Perform some login process for the user
   const login = function(email, password) {
-    setAuth(true);
     const id = "1234-1234-1234";
-    setUser({ email, id, name: "Test User" });
+    setUser({email, id, name: "Test User"});
   };
 
   const logout = function() {
-    setAuth(false);
     setUser(null);
   };
 
   return (
     <div className="App">
       <h1>My App</h1>
-      {!auth && <Login login={login} />}
-      {auth && <Info logout={logout} user={user} />}
+      {!user && <Login login={login} />}
+      {!!user && <Info logout={logout} user={user} />}
     </div>
   );
 }
